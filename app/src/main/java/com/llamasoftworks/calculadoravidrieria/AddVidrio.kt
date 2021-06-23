8deea90c26f7be2903988101d6c9f54355f5a52f
xiriuxb <jorge.trujillo@epn.edu.ec>
2021-06-23 15:50:03 -0500

    refactorizado clase addVidrio y lamina
    le incrementó el segundo precio en lámina pero todavía no esta usado

--- app/src/main/java/com/llamasoftworks/calculadoravidrieria/AddVidrio.kt
+++ app/src/main/java/com/llamasoftworks/calculadoravidrieria/AddVidrio.kt
@@ -1,24 +1,19 @@
 package com.llamasoftworks.calculadoravidrieria
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.annotation.RequiresApi
-import com.google.android.material.textfield.TextInputEditText
 import com.llamasoftworks.calculadoravidrieria.clases.Lamina
+import kotlinx.android.synthetic.main.activity_add_vidrio.*
 
 class AddVidrio : AppCompatActivity() {
 
-    var lamina:Lamina = Lamina("",0.0,0.0,0.0,0.0)
+    var lamina:Lamina = Lamina("")
     var numeroEncontrado = -1
 
-    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vidrio)
         window.setLayout(1100,850)
-
     }
 
     override fun onStart() {
@@ -28,40 +23,29 @@ class AddVidrio : AppCompatActivity() {
             lamina = VidriosFragment.list[numeroEncontrado]
             loadData()
         }
-        val btnGuardar = findViewById<Button>(R.id.button2)
-        btnGuardar.setOnClickListener {
+        button2.setOnClickListener {
             saveNewVidrio()
             finish()
         }
     }
 
     fun saveNewVidrio(){
-        val nombre = findViewById<TextInputEditText>(R.id.vidrio_nombre).text.toString()
-        val alto = findViewById<TextInputEditText>(R.id.vidrio_alto_l).text.toString().toDouble()
-        val ancho = findViewById<TextInputEditText>(R.id.vidrio_ancho_l).text.toString().toDouble()
-        val precioTotal = findViewById<TextInputEditText>(R.id.vidrio_precio_t).text.toString().toDouble()
-        val precioMetro = findViewById<TextInputEditText>(R.id.vidrio_precio_m).text.toString().toDouble()
-        if (lamina.nombre==""){
-            VidriosFragment.list.add(
-                    Lamina( nombre,alto,ancho,precioTotal,precioMetro )
-            )
-        }else{
-            lamina.nombre = nombre
-            lamina.alto = alto
-            lamina.ancho = ancho
-            lamina.precioT = precioTotal
-            lamina.precioM = precioMetro
-            VidriosFragment.list[numeroEncontrado]=lamina
+        val nombre = vidrio_nombre.text.toString()
+        val alto = vidrio_alto_l.text.toString().toDouble()
+        val ancho = vidrio_ancho_l.text.toString().toDouble()
+        val precioTotal = vidrio_precio_t.text.toString().toDouble()
+        val precioMetro = vidrio_precio_m.text.toString().toDouble()
+        when (lamina.nombre){
+            ""->VidriosFragment.list.add(Lamina(nombre,alto,ancho,precioTotal,precioMetro))
+            else -> VidriosFragment.list[numeroEncontrado]= Lamina(nombre, alto, ancho, precioTotal, precioMetro)
         }
     }
 
     private fun loadData(){
-        findViewById<TextInputEditText>(R.id.vidrio_nombre).setText(lamina.nombre)
-        findViewById<TextInputEditText>(R.id.vidrio_alto_l).setText(lamina.alto.toString())
-        findViewById<TextInputEditText>(R.id.vidrio_ancho_l).setText(lamina.ancho.toString())
-        findViewById<TextInputEditText>(R.id.vidrio_precio_t).setText(lamina.precioT.toString())
-        findViewById<TextInputEditText>(R.id.vidrio_precio_m).setText(lamina.precioM.toString())
+        vidrio_nombre.setText(lamina.nombre)
+        vidrio_alto_l.setText(lamina.alto.toString())
+        vidrio_ancho_l.setText(lamina.ancho.toString())
+        vidrio_precio_t.setText(lamina.precioT.toString())
+        vidrio_precio_m.setText(lamina.precioM.toString())
     }
-
-
 }
--- app/src/main/java/com/llamasoftworks/calculadoravidrieria/clases/Lamina.kt
+++ app/src/main/java/com/llamasoftworks/calculadoravidrieria/clases/Lamina.kt
@@ -4,9 +4,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Lamina(
     var nombre:String,
-    var alto: Double,
-    var ancho: Double,
-    var precioT:Double,
-    var precioM:Double
+    var alto: Double = 0.0,
+    var ancho: Double = 0.0,
+    var precioT:Double = 0.0,
+    var precioM:Double = 0.0,
+    var precioDos:Double = 0.0
         ){
 }
